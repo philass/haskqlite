3e2957bafba00450138edb57c03af682521e13ca
Philip Lassen <philiplassen@gmail.com>
2019-11-14 11:13:56 +0100

    Preliminary outline of REPL

---
+++ src/REPL.hs
@@ -0,0 +1,28 @@
+import Control.Monad
+import System.IO
+read_ :: IO String
+read_ = putStr "REPL> "
+     >> hFlush stdout
+     >> getLine
+
+
+
+eval_ :: String -> String
+eval_ input = input
+
+print_ :: String -> IO ()
+print_ = putStrLn
+
+
+
+
+main :: IO ()
+main = do
+  input <- read_
+  unless (input == ":quit")
+     $ print_ (eval_ input) 
+    >> main
+
+
+
+
